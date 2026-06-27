c36577342f5971098b7a8b207855ccb12cd14130
shubhamsgupta3 <shubham.gupta8@soprasteria.com>
2026-06-27 14:27:18 +0530

    Added production environment

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.prod';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-  private baseUrl = 'http://localhost:8000';
+  private baseUrl = environment.apiUrl; // 'http://localhost:8000'
 
   constructor(private http: HttpClient) { }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://railway-agent-ui.vercel.app'
 };
